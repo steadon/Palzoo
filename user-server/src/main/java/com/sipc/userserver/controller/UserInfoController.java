fc18dbdddd8fcd015b6e51b87bebc013a552e7de
DoudiNCer <doudi@outlook.lv>
2023-04-04 19:53:01 +0800

    Fix: User Service 取消跨域请求相关配置

--- user-server/src/main/java/com/sipc/userserver/controller/UserInfoController.java
+++ user-server/src/main/java/com/sipc/userserver/controller/UserInfoController.java
@@ -12,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
-@CrossOrigin
 @RestController
 @RequestMapping("/user")
 public class UserInfoController {
