cfb428ed067202776b6acbdf6e28435b756cd37d
DoudiNCer <doudi@outlook.lv>
2023-04-04 20:12:25 +0800

    Feat: User Service 完成获取所有学院专业信息接口

---
+++ user-server/src/main/java/com/sipc/userserver/service/impl/AcaMajorServiceImpl.java
@@ -0,0 +1,40 @@
+package com.sipc.userserver.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.sipc.userserver.mapper.AcaMajorMapper;
+import com.sipc.userserver.pojo.CommonResult;
+import com.sipc.userserver.pojo.domain.AcaMajor;
+import com.sipc.userserver.pojo.result.AcaMajorInfo;
+import com.sipc.userserver.service.AcaMajorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class AcaMajorServiceImpl implements AcaMajorService {
+    private final AcaMajorMapper acaMajorMapper;
+
+    @Autowired
+    public AcaMajorServiceImpl(AcaMajorMapper acaMajorMapper) {
+        this.acaMajorMapper = acaMajorMapper;
+    }
+
+    /**
+     * @return 所有学院与专业
+     */
+    @Override
+    public CommonResult<List<AcaMajorInfo>> getAllAcamajorInfo() {
+        List<AcaMajorInfo> result = new ArrayList<>();
+        List<AcaMajor> acaMajors = acaMajorMapper.selectList(new QueryWrapper<>());
+        for (AcaMajor acaMajor : acaMajors){
+            AcaMajorInfo info = new AcaMajorInfo();
+            info.setId(acaMajor.getId());
+            info.setAcaName(acaMajor.getAcaName());
+            info.setMajorName(acaMajor.getMajorName());
+            result.add(info);
+        }
+        return CommonResult.success(result);
+    }
+}
