65c7464ba75dcbc7e94b3f515aa13bc210043a35
DoudiNCer <doudi@outlook.lv>
2023-04-05 15:48:46 +0800

    Feat: Control Server 添加 User Server 的接口

---
+++ control-server/src/main/java/com/sipc/controlserver/pojo/result/userServer/AcaMajorInfo.java
@@ -0,0 +1,10 @@
+package com.sipc.controlserver.pojo.result.userServer;
+
+import lombok.Data;
+
+@Data
+public class AcaMajorInfo {
+    private Integer id;
+    private String acaName;
+    private String majorName;
+}
