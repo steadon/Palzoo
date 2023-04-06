c34f018f8d4ad26f188951ac4a17761630f8e994
DoudiNCer <doudi@outlook.lv>
2023-04-06 10:29:18 +0800

    Chore: Team Server 初始化 MyBatis Plus

---
+++ team-server/src/main/java/com/sipc/teamserver/mapper/TeamMapper.java
@@ -0,0 +1,18 @@
+package com.sipc.teamserver.mapper;
+
+import com.sipc.teamserver.pojo.domain.Team;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author DoudiNCer
+ * @since 2023-04-06
+ */
+@Mapper
+public interface TeamMapper extends BaseMapper<Team> {
+
+}
