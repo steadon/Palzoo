4cb65ec48e53d138b375e6f2f7b159c2c67cb1bf
tangzihan <1922941549@qq.com>
2023-04-11 11:48:45 +0800

    fix(topic-server&message-server):修复了提交帖子重复消费的问题

--- message-server/src/main/java/com/sipc/messageserver/MessageServerApplication.java
+++ message-server/src/main/java/com/sipc/messageserver/MessageServerApplication.java
@@ -1,6 +1,5 @@
 package com.sipc.messageserver;
 
-import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
